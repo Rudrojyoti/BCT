d4a7f72f6910e0cc595142f3e84215d29bbf824f
MannaRudrojyoti_24 <mannarudrojyoti@gmail.com>
2025-02-27 16:58:53 +0530

    Add files via upload

---
+++ Day 4/task2.py
@@ -0,0 +1,16 @@
+d={
+    "Name":"Rudrojyoti Manna",
+    "Stream":"CSE",
+    "Course":"BTECH"
+}
+print(d)
+l=["student profile"]
+print(d.get("Name"))
+print(d.get("Stream"))
+print(d.get("Course"))
+print(d.keys())
+print(d.values())
+print(d.items())
+print(d.popitem())
+print(d.clear())
+print(d)
